2c0515b965d3a49beafec168b58531ee891c496c
lisi <1786678583@qq.com>
2020-10-18 18:03:25 +0800

    sseeFDSS

--- src/aa.java
+++ src/aa.java
@@ -1,5 +1,6 @@
 public class aa {
     public static void main(String[] args) {
         System.out.println("333333remote");
+        System.out.println("333333local");
     }
 }
--- src/dd.java
+++ src/dd.java
@@ -1,5 +1,7 @@
 public class dd {
     public static void main(String[] args) {
         System.out.println(1112222);
+        System.out.println("1111LOCAL");
+
     }
 }
